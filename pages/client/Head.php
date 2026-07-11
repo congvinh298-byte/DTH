b7b69076e92a80ff39f8566aebe4c9b7796f6100
congvinh298-byte <congvinh298@gmail.com>
2026-07-11 17:55:35 +0700

    Fix Swal is not defined error by adding sweetalert2 to Head

--- pages/client/Head.php
+++ pages/client/Head.php
@@ -38,5 +38,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.11/clipboard.min.js"></script>
     <script src="/assets/js/toastr.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     
 </head>
