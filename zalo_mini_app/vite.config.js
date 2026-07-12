cd3d6eedac53295dd0af117cab8fdc2ecc4e4bdb
congvinh298-byte <congvinh298@gmail.com>
2026-07-12 18:19:42 +0700

    Create pure HTML wrapper for Zalo Mini App URL Hosting

--- zalo_mini_app/vite.config.js
+++ zalo_mini_app/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react';
+import zmpVitePlugin from 'zmp-vite-plugin';
 
 export default () => {
   return defineConfig({
     base: '',
-    plugins: [reactRefresh()]
+    plugins: [reactRefresh(), zmpVitePlugin()]
   });
 };
